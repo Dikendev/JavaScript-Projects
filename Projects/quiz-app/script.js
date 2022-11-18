2bd7d670d67bd6cb5f9ff72ea1843453cebdfe81
diken.dev <diken.dev@gmail.com>
2022-11-17 23:20:27 -0300

    index quiz app

---
+++ Projects/quiz-app/script.js
@@ -0,0 +1,40 @@
+const quizData = [
+    {
+        question: " De quem é a famosa frase “Penso, logo existo”?",
+        a: "Platão",
+        b: "Galileu Galilei",
+        c: "Descartes",
+        d: "Sócrates",
+        correct: "c"
+    }, {
+        question: "Atualmente, quantos elementos químicos a tabela periódica possui?",
+        a: "113",
+        b: "109",
+        c: "108",
+        d: "118",
+        correct: "d"
+    }, {
+        question: " Quanto tempo a luz do Sol demora para chegar à Terra",
+        a: "12 minutos",
+        b: "1 dia",
+        c: "12 horas",
+        d: "8 min",
+        correct: "d"
+    }, {
+        question: "Em que período da pré-história o fogo foi descoberto?",
+        a: "Neolítico",
+        b: "Paleolítico",
+        c: "Idade dos Metais",
+        d: "Período da Pedra Polida",
+        correct: "b"
+    }, {
+        question: "Qual a velocidade da luz?",
+        a: "300 000 000 metros por segundo (m/s)",
+        b: "150 000 000 metros por segundo (m/s)",
+        c: "199 792 458 metros por segundo (m/s)",
+        d: "299 792 458 metros por segundo (m/s)",
+        correct: "d"
+    }
+    
+    
+]
